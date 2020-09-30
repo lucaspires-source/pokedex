2206dddc1f2ae383428244ccea7d8d337c99b6b7
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-30 16:15:55 -0300

    stylling main page

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -8,6 +8,21 @@ const Sprite = styled.img`
     display:none
     `;
 
+    const Card = styled.div`
+    opacity: 0.95;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    }
+    -moz-user-select: none;
+    -website-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -o-user-select: none;
+  `;
+  
+
 
 export default class PokemonCard extends Component {
 
@@ -30,35 +45,36 @@ export default class PokemonCard extends Component {
 
         return (
             <div className="col-md-3 col-sm-6 mb-5">
-                <div className="card">
-                        <h5 className="card-header">{this.state.pokemonIndex}</h5>
-                        {this.state.imageLoading ? (
-                            <img  
-                                src={spinner}
-                                style={{ width: '5em', height: '5em' }}
-                                className="card-img-top rounded mx-auto d-block mt-2"
-                                alt=""
+                    <Card className="card">
+                            <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                            {this.state.imageLoading ? (
+                                <img  
+                                    src={spinner}
+                                    style={{ width: '5em', height: '5em' }}
+                                    className="card-img-top rounded mx-auto d-block mt-2"
+                                    alt=""
+                                />
+                            ): null}
+                            <Sprite 
+                                className="card-img-top rounded mx-auto mt-2" 
+                                onLoad= {()=> this.setState({imageLoading:false})}
+                                onError={()=> this.setState({toManyRequest:true})}
+                                src={this.state.imageUrl}
+                                style={
+                                    this.state.toManyRequest ? {display:"none"}
+                                    : this.state.imageLoading ? null : {display:"block"}
+                                }
                             />
-                        ): null}
-                        <Sprite 
-                            className="card-img-top rounded mx-auto mt-2" 
-                            onLoad= {()=> this.setState({imageLoading:false})}
-                            onError={()=> this.setState({toManyRequest:true})}
-                            src={this.state.imageUrl}
-                            style={
-                                this.state.toManyRequest ? {display:"none"}
-                                : this.state.imageLoading ? null : {display:"block"}
-                            }
-                        />
-                        {this.state.toManyRequest ? (
-                            <h6 className="mx-auto">
-                                <span className="badge badge-danger mt-2">To Many Requests</span>
-                            </h6>
-                        ) : null }
-                        <div className="card-body">
-                            <h6 className="card-title" class="text-capitalize" >{this.state.name}</h6>
-                        </div>
-                </div>
+                            {this.state.toManyRequest ? (
+                                <h6 className="mx-auto">
+                                    <span className="badge badge-danger mt-2">To Many Requests</span>
+                                </h6>
+                            ) : null }
+                            <div className="card-body">
+                                <h6 className="card-title" class="text-capitalize" >{this.state.name}</h6>
+                            </div>
+                    </Card>
+                
             </div>
         )
     }
