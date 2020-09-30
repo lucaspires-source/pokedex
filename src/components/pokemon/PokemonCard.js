6fd8aa9e84b9da9be5e2cfa59e6ef3c3c3213250
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2020-09-30 15:23:22 -0300

    images were added to the cards

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
+import styled from "styled-components"
+import spinner from "../pokemon/Spinning.gif"
+const Sprite = styled.img`
+
+    width:5em;
+    height:5em;
+    display:none
+    `;
+
+
 export default class PokemonCard extends Component {
 
     state ={
         name:'',
         imageUrl:'',
-        pokemonIndex:''
+        pokemonIndex:'',
+        imageLoading:true,
+        toManyRequest:false
     }
 
     componentDidMount(){
@@ -21,6 +32,29 @@ export default class PokemonCard extends Component {
             <div className="col-md-3 col-sm-6 mb-5">
                 <div className="card">
                         <h5 className="card-header">{this.state.pokemonIndex}</h5>
+                        {this.state.imageLoading ? (
+                            <img  
+                                src={spinner}
+                                style={{ width: '5em', height: '5em' }}
+                                className="card-img-top rounded mx-auto d-block mt-2"
+                                alt=""
+                            />
+                        ): null}
+                        <Sprite 
+                            className="card-img-top rounded mx-auto mt-2" 
+                            onLoad= {()=> this.setState({imageLoading:false})}
+                            onError={()=> this.setState({toManyRequest:true})}
+                            src={this.state.imageUrl}
+                            style={
+                                this.state.toManyRequest ? {display:"none"}
+                                : this.state.imageLoading ? null : {display:"block"}
+                            }
+                        />
+                        {this.state.toManyRequest ? (
+                            <h6 className="mx-auto">
+                                <span className="badge badge-danger mt-2">To Many Requests</span>
+                            </h6>
+                        ) : null }
                         <div className="card-body">
                             <h6 className="card-title" class="text-capitalize" >{this.state.name}</h6>
                         </div>
